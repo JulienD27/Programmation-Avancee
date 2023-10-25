ceed67e20f612291ce145e319d78efdf6f74d30f
jd594217 <juliendurand77140@gmail.com>
2023-10-25 16:21:31 +0200

    question 3.2

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,14 +2,15 @@ import { BreadCrumbs, SectionContainer } from "tp-kit/components";
 import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
 import { ProductList } from "../components/product-list";
 import { Metadata } from "next";
-const categories = PRODUCTS_CATEGORY_DATA;
+import prisma from "../utils/prisma";
+const categories = await prisma.productCategory.findMany({ include: { products: true } });
 
 export const metadata:Metadata = {
   title: `Page d’accueil - Starbucks`,
   description: "Commandez de délicieuses boissons préparées avec soin par nos baristas"
 }
 
-export default function Home() {
+export default async function Home() {
   return (<SectionContainer>
     <BreadCrumbs items={[
       {
