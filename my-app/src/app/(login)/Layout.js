8a2c5e4c24be20265e916f1d8cc3b112ae7eeef0
Julien DURAND <jd594217@iut21.u-bourgogne.fr>
2023-11-22 15:28:50 +0100

    2.0

---
+++ my-app/src/app/(login)/Layout.js
@@ -0,0 +1,14 @@
+// components/Layout.js
+import React from 'react';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <header>Header</header>
+            <main>{children}</main>
+            <footer>Footer</footer>
+        </div>
+    );
+};
+
+export default Layout;
